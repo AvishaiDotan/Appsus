219d881b718f28cce4fd58f58a30a90908ff29ca
Avishai Dotan <avishaiDotan@gmail.com>
2022-11-09 10:31:21 +0200

    wiring up git

--- cmps/app-header.cmp.js
+++ cmps/app-header.cmp.js
@@ -4,7 +4,9 @@ export default {
             <h1>AppSus</h1>
             <nav>
                 <router-link to="/">Home</router-link> | 
-                <router-link to="/about">About</router-link>
+                <router-link to="/about">About</router-link> |
+                <router-link to="/mail">Mail</router-link> |
+                <router-link to="/Keep">Keep</router-link> |
             </nav>
         </header>
     `,
--- routes.js
+++ routes.js
@@ -1,5 +1,7 @@
 import homePage from './views/app-home.cmp.js'
 import aboutPage from './views/app-about.cmp.js'
+import mailApp from './apps/mail/cmps/mail-app.cmp.js'
+import keepApp from './apps/keep/cmps/keep-app.cmp.js'
 
 const { createRouter, createWebHashHistory } = VueRouter
 
@@ -13,6 +15,14 @@ const routerOptions = {
 		{
 			path: '/about',
 			component: aboutPage,
+		},
+		{
+			path: '/mail',
+			component: mailApp,
+		},
+		{
+			path: '/keep',
+			component: keepApp,
 		},
 	],
 }

c151631013b6b15ac0cbdd2d1cece652f4e48b61
Avishai Dotan <avishaiDotan@gmail.com>
2022-11-12 21:57:38 +0200

    add fix about page layout

--- apps/mail/cmps/email-header.cmp.js
+++ apps/mail/cmps/email-header.cmp.js
@@ -3,7 +3,7 @@ import { eventBus } from "../../../services/event-bus.service.js"
 
 export default {
     template: `
-            <img class="email-logo" src="../assets/style/apps/mail/icons/gmail-logo-icon.png" alt="email-logo" />
+            <img class="email-logo" src="./assets/style/apps/mail/icons/gmail-logo-icon.png" alt="email-logo" />
             <div class="main-header-features">
                 <email-search class="email-search"/>
                 <img @click="toggleMailSideMenu" class="mail-mobile-side-menu" src="./assets/style/apps/mail/icons/appsus-grid-icon.png" alt="menu-btn" />
